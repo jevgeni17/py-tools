4dba2632f1d2558849bc3092ceffb19ae07db998
Jevgeni Barkin <zekaxdpro@gmail.com>
2020-03-20 21:29:42 +0200

    new functions

--- priceChecker/views.py
+++ priceChecker/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserForm
-import time
-from selenium import webdriver
 
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import time
+import requests
   
 def index(request):
     return render(request, "index.html")
 
-links_list = ['rahvaraamat.ee/s/{}/ru?productType=&keyword={}', 'bookvoed.ee/search?q={}', 'mnogoknig.ee/search/{}']
+links_list = ['rahvaraamat.ee/search/productList/en?offset=0&limit=10&searchTerm={}', 'bookvoed.ee/search?q={}&count=50#page-1', 'mnogoknig.ee/search/{}']
 
-def createURL(keywordsList,urlExample):
+def createURL(keywordsList,urlExample): # returns a serach url with user keywords
     """ NOTICE!  white fang  - there is a book title example (which user will write in the form as a keyword)
-        urlExample -             rahvaraamat.ee/s/{}/ru?productType=&keyword={} - where {} there are search keywords
-        function will return =>  rahvaraamat.ee/s/white+fang/ru?productType=&keyword=white+fang  - instead of  {} will paste search keywords
+        urlExample -             rahvaraamat.ee/search/productList/en?offset=0&limit=10&searchTerm={} - where {} there are search keywords
+        function will return =>  rahvaraamat.ee/search/productList/en?offset=0&limit=10&searchTerm=мартин+иден  - instead of  {} will paste search keywords
                                  bookvoed.ee/search?q={} =>  bookvoed.ee/search?q=white+fang
                                  mnogoknig.ee/search/{}  =>   mnogoknig.ee/search/white+fang
 
@@ -22,37 +25,47 @@ def createURL(keywordsList,urlExample):
     url = 'https://' + urlExample.replace("{}",plusKeywords)
     return url
 
-def crawl_data(url,product_link_class,price_class):
-    item_dict = {}
-
-    browser = webdriver.Chrome('chromedriver')
-    browser.get(url)  
-    time.sleep(4)
-    html_source = browser.page_source  
+def item_url(page,class1,secondClass=None): # returns list of links for each item in search page.
+    #.title > a.js-link-product     - rahvaraamat
+    #.o-row > a.title               - bookvoed
+    #.col-xs-8 > a                  - mnogoknig
+    page = requests.get(page)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    links = []
     
-    
+    if secondClass == None:
+        string = f".{class1} > a"
+    else:
+        string = f".{class1} > a.{secondClass}"
 
-    #soup = BeautifulSoup(html_source,'html.parser')  
-    #class "postText" is not defined in the source code
-    #titles = soup.findAll('p',{'class':'title'})
-    #titles = browser.find_elements_by_xpath("//*[@id='endlessProducts']/li[1]/div/div/div/div[2]/div[1]/p[2]")
-    #for link in titles:
-     #   print (link.get_attribute("href"))
-    elems = browser.find_elements_by_css_selector(".title [href]")
-    links = [elem.get_attribute('href') for elem in elems]
+    for a in soup.select(string):
+        href = urljoin(page.url, a.get('href'))
+        links.append(href)
 
-    prices = browser.find_elements_by_class_name("price")
-    price = [i.text for i in prices]
-    #print(price)
-    #for i in price:
-    #    new_price = []
-    #    x = i.partition('\n')[0]
-    #    new_price.append(x)
-    browser.quit()
-    new_price = [i.partition('\n')[0] for i in price] # delete all after '\n'
-    time.sleep(2)
-    return new_price
-    
+    return links
+
+def item_price(page,class1,secondClass=None): # returns list of prices for each item in search page
+    # .meta > p.price - rahvaraamat
+    # .buy > span     - bookvoed
+    # .price          -mnogoknig
+    page = requests.get(page)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    prices = []
+    if secondClass == None:
+        string = f".{class1}"
+    elif secondClass == 'span':
+        string = f".{class1} > {secondClass}"
+    elif secondClass == 'price':
+        string = f".{class1} > p.{secondClass}"
+
+    for p in soup.select(string):
+        p =  p.get_text()
+        prices.append(p)
+    prices = [i.partition(' €')[0] for i in prices] # delete unnecessary symbols
+    prices = [i.strip() for i in prices] # delete whitespaces
+    prices = [i.replace('€','') for i in prices] # delete unnecessary symbols
+    return prices
+
 
 
     
@@ -61,8 +74,6 @@ def priceChecker(request):
     if request.method == 'POST':
         name = request.POST.get("name")
         user_keywords = name.split(" ") 
-        test = createURL(user_keywords,links_list[1])
-        test1 = crawl_data('https://www.rahvaraamat.ee/s/%D1%82%D1%80%D0%B8-%D1%82%D0%BE%D0%B2%D0%B0%D1%80%D0%B8%D1%89%D0%B0/ru?productType=&keyword=%D1%82%D1%80%D0%B8+%D1%82%D0%BE%D0%B2%D0%B0%D1%80%D0%B8%D1%89%D0%B0','js-link-product','price')
         return HttpResponse(f"<a href={test1}>{test1}</a>")
     else:
         userform = UserForm()
